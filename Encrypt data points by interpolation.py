4f0a04c9e2389fc7c11dafe7562936ff86919d52
Fei Han <59255164+hanfei1986@users.noreply.github.com>
2023-08-27 13:22:28 -0400

    Add files via upload

--- Encrypt data points by interpolation.py
+++ Encrypt data points by interpolation.py
@@ -8,22 +8,22 @@ path = 'C:/Users/fhan/Desktop/Encrypt data points by interpolation\*.xlsx'
 for file in glob.glob(path):
     filename = file.split('\\')[1].split('.')[0]
     df = pd.read_excel(file)
-    x = df['x']
-    y = df['y']
-    
-    new_x = np.arange(x.min(), x.max()+1, 1)
+    new_df = pd.DataFrame()
 
-    print(new_x)
-    
-    f_xy = interp1d(x, y, kind='cubic')
+    if len(df.columns)>=1:
+        x = df[df.columns[0]]
+        new_x = np.arange(x.min(), x.max()+1, 1)
+        new_df[df.columns[0]] = new_x
+        for i in range(1,len(df.columns)):
+            y = df[df.columns[i]]
+            f_xy = interp1d(x, y, kind='cubic')
+            new_y = f_xy(new_x)
+            new_df[df.columns[i]] = new_y
 
-    new_y = f_xy(new_x)
+            plt.scatter(x, y, c='blue', s=50)
+            plt.plot(new_x, new_y, c='red', marker='.')
+            plt.xlabel(df.columns[0])
+            plt.ylabel(df.columns[i])
+            plt.show()
 
-    plt.scatter(x, y)
-    plt.plot(new_x, new_y)
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.show()
-
-    new_df = pd.DataFrame({'x': new_x, 'y': new_y})
-    new_df.to_excel('C:/Users/fhan/Desktop/Encrypt data points by interpolation/{} interpolated.xlsx'.format(filename), index=False)
+        new_df.to_excel('C:/Users/fhan/Desktop/Encrypt data points by interpolation/{} interpolated.xlsx'.format(filename), index=False)
